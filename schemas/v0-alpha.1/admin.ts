a28c52f57e0375701f81b63fa260c21225a7c59b
AnasMubarakYasin <bladerlaiga.97@gmail.com>
2021-10-18 17:58:48 +0800

    update admin schema

--- schemas/v0-alpha.1/admin.ts
+++ schemas/v0-alpha.1/admin.ts
@@ -5,9 +5,22 @@ import { Data as Item } from './selected-item';
 import { Data as Product } from './product';
 import { Data as Store } from './store';
 import { Data as Order } from './order';
+import { Data as OrderedItem } from './ordered-item';
+import { Data as Delivery } from './delivery';
+import { Data as Courier } from './courier';
+import { Data as Rating } from './rating';
 
 export type Data = typeof D;
-export type { Buyer, BuyerAddress, Item, Product, Store, Order };
+export type {
+	Buyer,
+	BuyerAddress,
+	Item,
+	Product,
+	Store,
+	Order,
+	OrderedItem,
+	Delivery,
+};
 
 export type Stat = {
 	user: number;
@@ -18,4 +31,58 @@ export type Stat = {
 export type SentEvent<D = any> = {
 	tag: string;
 	data: D;
+};
+export type Slide = {
+	id: number;
+	link: string;
+	src: string;
+}
+export type User = {
+	id: number;
+	node: number;
+	role: string;
+	username: string;
+	email: string;
+	telp?: string | null;
+	address?: string | null;
+	image?: string | null;
+};
+export type ProductData = Product & {
+	store: Store;
+};
+export type OrderData = Order & {
+	item: (OrderedItem & { product: Product })[];
+	delivery: Delivery & { sender: Store; recipient: BuyerAddress };
+};
+export type OrderDataDetail = Order & {
+	item: (OrderedItem & { product: Product })[];
+	delivery: Delivery & {
+		sender: Store;
+		recipient: BuyerAddress & { buyer: Buyer | null };
+		courier: Courier | null;
+	};
+};
+export type Sale = Order & {
+	store: Store;
+	delivery: Delivery;
+	item: (OrderedItem & {
+		product: Product;
+	})[];
+	rating:
+		| (Rating & {
+				buyer: Buyer;
+		  })
+		| null;
+};
+export type SaleDetail = Order & {
+	buyer: Buyer;
+	delivery: Delivery & {
+		sender: Store;
+		recipient: BuyerAddress;
+		courier: Courier | null;
+	};
+	item: (OrderedItem & {
+		product: Product;
+	})[];
+	rating: Rating | null;
 };
