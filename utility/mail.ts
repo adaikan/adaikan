82f63f7182f25ad8f0a66f98f7642d2d344d8bb4
AnasMubarakYasin <bladerlaiga.97@gmail.com>
2021-10-31 02:13:19 +0800

    update: utility mail

--- utility/mail.ts
+++ utility/mail.ts
@@ -7,6 +7,7 @@ import fs from 'fs';
 import path from 'path';
 import chalk from 'chalk';
 import { SMTPServer } from 'smtp-server';
+import { simpleParser } from 'mailparser';
 import { createTransport, Transporter } from 'nodemailer';
 
 export interface Options extends ListenOptions {
@@ -26,8 +27,9 @@ export interface SendOptions {
 		| (string | { name: string; address: string })[];
 	replyTo?: string | { name: string; address: string };
 	priority?: 'high' | 'low' | 'normal';
-	html: string;
-	test?: string;
+	subject?: string;
+	html?: string;
+	text?: string;
 }
 
 export const defOpts: Options = {
@@ -41,7 +43,8 @@ export const defOpts: Options = {
 export class Mail {
 	public static server: SMTPServer;
 	public static client: Transporter;
-	public static url = 'smtp://username:password@smtp.example.com/?pool=true';
+	public static client_url =
+		'smtp://username:password@smtp.example.com/?pool=true';
 	public static options: Options;
 	public static setup(opts: Options) {
 		this.options = Object.assign({}, defOpts, opts) as Required<Options>;
@@ -49,12 +52,44 @@ export class Mail {
 		if (!app) {
 			throw new Error('App on options not exist');
 		}
-		this.url = `${url}/?pool=true`;
-		this.client = createTransport(this.url, {
-			logger: true,
+		this.server = new SMTPServer({
+			logger: this.options.verbose,
+			onData(stream, session, cb) {
+				stream.once('close', cb);
+				simpleParser(stream, (error, message) => {
+					if (error) {
+						app.log.error(error);
+					} else {
+						app.log.info(message);
+					}
+				});
+			},
+			disabledCommands: ['AUTH'],
+		});
+		this.client_url = `${url}/?pool=true`;
+		this.client = createTransport(this.client_url, {
+			secure: false,
+			logger: this.options.verbose,
 		});
 
 		console.log(chalk.bgBlack.white`Mail Server Created`, chalk.green`[*]`);
+	}
+	public static listen(port?: number, host?: string) {
+		this.server.listen(port ?? this.options.port, host ?? this.options.host);
+		this.server.server.once('listening', () => {
+			const address = this.server.server.address() as {
+				address: string;
+				family: string;
+				port: number;
+			};
+			if (!address) {
+				throw new Error('Mail address ' + address);
+			}
+			this.options.app.log.info(
+				chalk`Mail Server Listening on ` +
+					chalk.blueBright`http://${address.address}:${address.port}`
+			);
+		});
 	}
 	public static async send(options: SendOptions) {
 		this.options.app.log.info(
