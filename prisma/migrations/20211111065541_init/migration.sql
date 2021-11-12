3cc71a28425d5704d1fa90dfb41723061c228c3a
AnasMubarakYasin <bladerlaiga.97@gmail.com>
2021-11-12 11:24:22 +0800

    update: schema orm

---
+++ prisma/migrations/20211111065541_init/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `proofImage` on the `Order` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Delivery" ADD COLUMN     "proofImage" TEXT;
+
+-- AlterTable
+ALTER TABLE "Order" DROP COLUMN "proofImage";
